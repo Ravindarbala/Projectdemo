775e9d9afaa5c66b48946b494315038efc31fe52
Your Name <you@example.com>
2021-07-07 12:02:10 +0530

    Barani commit

---
+++ src/sample/Github.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Github {
+	
+	public static void password() {
+		// TODO Auto-generated method stub
+System.out.println("Pass");
+	}
+public static void main(String[] args) {
+	password();
+	
+}
+}
